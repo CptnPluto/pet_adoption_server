74a662998fafbabfc90a896525bebaa8cd8555f1
CptnPluto <cptnpluto@gmail.com>
2023-01-18 14:17:24 +0200

    One more try.

--- migrations/20230105131705_add_petsList_table.js
+++ migrations/20230105131705_add_petsList_table.js
@@ -1,7 +1,9 @@
 exports.up = function (knex) {
     return knex.schema.createTable("usersPetsList", (table) => {
+        table.increments("id").primary().notNullable();
         table.integer("userId").unsigned().notNullable();
         table.integer("petId").unsigned().notNullable();
+        table.timestamp("created_at").defaultTo(knex.fn.now());
     });
 };
 
